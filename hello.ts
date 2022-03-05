7fff0a9d6e56866f92ae5576e4f8faa5f7f1d115
Freewind <nowindlee@gmail.com>
2022-03-05 13:23:28 +0800

    typescript-node-write-stream-to-file-demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,11 @@
-console.log('Hello');
+import fs from 'fs';
+
+const logFile = fs.createWriteStream('log.txt')
+
+for (let i = 0; i < 10000; i++) {
+  process.nextTick(() => logFile.write(`${i} - hello\n`))
+}
+
+process.nextTick(() => {
+  logFile.end()
+})
